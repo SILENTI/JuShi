a21efaded9231d6878a782cd88afd4a441cc55fa
SILENTI <fruitboxs@outlook.com>
2021-10-25 12:14:58 +0800

    商品展示

--- src/main/java/com/example/jushi/controller/GoodsController.java
+++ src/main/java/com/example/jushi/controller/GoodsController.java
@@ -1,16 +1,12 @@
 package com.example.jushi.controller;
 
-import com.example.jushi.model.Goods;
 import com.example.jushi.service.GoodsService;
 import com.example.jushi.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -20,11 +16,13 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
-    @GetMapping("/")
+    @GetMapping("/JuShi")
     public String goods (Model model){
         List<GoodsVo> goodsVos = goodsService.goodsShowAll();
         model.addAttribute("GoodsVo",goodsVos);
         return "GoodsShow";
     }
+
+
 
 }
